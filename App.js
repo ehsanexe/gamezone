18106bf26ae7c6b67123f0ffcd9e6b6e053d52f0
ehsanexe <ehsanexe01@gmail.com>
2020-10-07 19:12:52 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import { AppNavigator } from "./routes/AppNavigator";
+
+export default function App() {
+  return (
+    <AppNavigator />
+  );
+}
+
---
+++ screens/reviewDetails.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import {globalStyles} from '../styles/global';
+
+export default function reviewDetails() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Review Details Screen</Text>
+    </View>
+  );
+}
+
