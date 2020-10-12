c8f14e8da23f5b8d5d6d1bfc8cc924d4d2c528e5
ehsanexe <ehsanexe01@gmail.com>
2020-10-12 18:36:47 +0500

    css added and images file

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import {globalStyles} from '../styles/global';
+import Card from '../shared/card'
 
 export default function reviewDetails({route}) {
   
@@ -9,9 +10,11 @@ export default function reviewDetails({route}) {
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>{title}</Text>
-      <Text style={globalStyles.titleText}>{rating}</Text>
-      <Text style={globalStyles.titleText}>{body}</Text>
+      <Card>
+        <Text style={globalStyles.titleText}>{title}</Text>
+        <Text style={globalStyles.titleText}>{rating}</Text>
+        <Text style={globalStyles.titleText}>{body}</Text>
+      </Card>
     </View>
   );
 }
